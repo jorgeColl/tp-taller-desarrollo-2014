9ff77a22520c70459755d7ea79e36d6442ea2105
jorgsmu@gmail.com <jorgsmu@gmail.com@3fdfc800-5695-3f95-0b7d-b56a697e1d9a>
2014-11-22 23:49:57 +0000

    Login y creacion de cookies temporales luego de ingresar.

--- trunk/bitwin/config/routes.rb
+++ trunk/bitwin/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
