c6ee94b8ff48e098ecd5d22f8fd24e15f1d2b426
jorgsmu@gmail.com <jorgsmu@gmail.com@3fdfc800-5695-3f95-0b7d-b56a697e1d9a>
2014-11-23 23:56:47 +0000

    Se generan las rutas para apuestas y la vista para listar apuestas por usuario

--- trunk/bitwin/db/schema.rb
+++ trunk/bitwin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123221924) do
+ActiveRecord::Schema.define(version: 20141123223240) do
 
   create_table "gambles", force: true do |t|
     t.integer  "user_id"
